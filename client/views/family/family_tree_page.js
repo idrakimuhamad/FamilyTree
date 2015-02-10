ea68bf009acd8aac13680cdfcffa04dfd179512e
idrakimuhamad <idrakimuhamad@gmail.com>
2015-02-10 18:03:51 +0800

    Got the tweak for the data
    So that we can return the data to client in a single tree document with
    all children embed into the parents.

--- client/views/family/family_tree_page.js
+++ client/views/family/family_tree_page.js
@@ -1,9 +1,23 @@
+var selectedPerson;
+
 Template.family_tree_page.helpers({
   family: function () {
     return Family.findOne({ _id: Session.get('currentTree') });
   },
   noPerson: function () {
     return People.find().count() == 0;
+  },
+  person: function () {
+    return People.find();
+  }
+});
+
+Template.family_tree_page.events({
+  'click .person': function (e, t) {
+    e.preventDefault();
+    var id = $(e.currentTarget).data('id');
+    selectedPerson = id;
+    $('.menu').toggleClass('hidden');
   }
 });
 
@@ -15,10 +29,30 @@ Template.create_root.events({
       name: t.find('.root').value,
       dob: t.find('.dob').value,
       familyId: Session.get('currentTree'),
-      isRoot: true
+      isRoot: true,
+      children: []
     };
 
     Meteor.call('createPeople', people, function (err, res) {
+      if (err) alert('Oppps... There seems to be a problem. ' + err.reason);
+      Tracker.flush();
+    });
+  }
+});
+
+Template.menu.events({
+  'submit #child': function (e, t) {
+    e.preventDefault();
+
+    var people = {
+      name: t.find('.child').value,
+      dob: t.find('.dob').value,
+      familyId: Session.get('currentTree'),
+      isRoot: false,
+      children: []
+    };
+
+    Meteor.call('addChild', people, selectedPerson, function (err, res) {
       if (err) alert('Oppps... There seems to be a problem. ' + err.reason);
       Tracker.flush();
     });
--- server/publish.js
+++ server/publish.js
@@ -3,8 +3,21 @@ Meteor.publish('family', function (userId) {
 });
 
 Meteor.publish('peoplesInFamily', function (familyId) {
-  return People.find({ familyId: familyId }, { 
-    transform : function (doc) {
-    }
-  });
+  var self = this;
+
+  People.find(
+    { familyId: familyId },
+    { fields: {name: 1, dob: 1, familyId: 1, children: 1} }
+    ).forEach(function (doc) {
+      childIntoChild(doc, self);
+    });
+
+  self.ready();
 });
+
+function childIntoChild (doc, self) {
+  if (doc.children.length) {
+    doc.children =  People.find({_id: {$in: doc.children}}, {fields: {name: 1, dob: 1, familyId: 1, children: 1}}).fetch();
+    self.added('people', doc._id, doc);
+  }
+}
